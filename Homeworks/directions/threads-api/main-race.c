49c5dd605af0cf35671f11e2acc989c80736f4ff
Tristan Giga <tristan.giga@free.fr>
2023-08-25 18:31:15 +0200

    Update

--- Homeworks/directions/threads-api/main-race.c
+++ Homeworks/directions/threads-api/main-race.c
@@ -1,18 +1,25 @@
+#include <pthread.h>
 #include <stdio.h>
 
 #include "common_threads.h"
 
 int balance = 0;
+pthread_mutex_t mutex1  ;
 
 void* worker(void* arg) {
-    balance++; // unprotected access 
+    Mutex_lock(&mutex1);
+    balance++;
+    Mutex_unlock(&mutex1);
     return NULL;
 }
 
 int main(int argc, char *argv[]) {
+    Mutex_init(&mutex1);
     pthread_t p;
     Pthread_create(&p, NULL, worker, NULL);
-    balance++; // unprotected access
+    Mutex_lock(&mutex1);
+    balance++;
+    Mutex_unlock(&mutex1);
     Pthread_join(p, NULL);
     return 0;
 }
